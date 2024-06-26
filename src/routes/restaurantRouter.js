f3ea181be0844d959acefdf8eb4de0274a0c100b
Minh Trung <minhtrungcm181@gmail.com>
2024-06-26 16:32:43 +0700

    dev

--- src/routes/restaurantRouter.js
+++ src/routes/restaurantRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getAllRestaurant } from "../controller/restaurantController.js";
+import { getAllRestaurant, getLikeByUser, getResLikeByUser, likeRestaurant, unlikeByUser } from "../controller/restaurantController.js";
 
 const restaurantRouter = express.Router();
 
 restaurantRouter.get("/all", getAllRestaurant)
 
+restaurantRouter.post("/like", likeRestaurant)
+
+restaurantRouter.post("/unlike", unlikeByUser)
+
+restaurantRouter.get("/get-like-by-user/:user_id", getLikeByUser)
+
+restaurantRouter.get("/get-restaurant-like-by-user/:user_id", getResLikeByUser)
 
 export default restaurantRouter
