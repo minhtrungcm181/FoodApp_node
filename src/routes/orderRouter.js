ceb5fd5ec20fe3ae45e3dedf29f27a911971330b
Minh Trung <minhtrungcm181@gmail.com>
2024-06-26 23:50:55 +0700

    finish

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { orderCreate } from "../controller/orderController.js";
 
 const orderRouter = express.Router();
+
+orderRouter.post("/orderCreate", orderCreate)
 
 
 
